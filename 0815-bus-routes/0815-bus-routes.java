86704939d89fba1bd31e33ca831494e4fccfc7ef
Hangsha <125459168+hangsha-saha@users.noreply.github.com>
2025-08-13 00:12:31 +0530

    Time: 333 ms (48.04%), Space: 78 MB (38.67%) - LeetHub

---
+++ 0815-bus-routes/0815-bus-routes.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int numBusesToDestination(int[][] routes, int source, int target) {
+        if (source == target) return 0;
+
+        int n = routes.length;
+        Map<Integer, List<Integer>> stopToRoutes = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            for (int stop : routes[i]) {
+                stopToRoutes.computeIfAbsent(stop, x -> new ArrayList<>()).add(i);
+            }
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        boolean[] visitedRoutes = new boolean[n];
+        Set<Integer> visitedStops = new HashSet<>();
+
+        for (int route : stopToRoutes.getOrDefault(source, new ArrayList<>())) {
+            queue.offer(route);
+            visitedRoutes[route] = true;
+        }
+
+        int busesTaken = 1;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+
+            for (int i = 0; i < size; i++) {
+                int routeIdx = queue.poll();
+
+                for (int stop : routes[routeIdx]) {
+                    if (stop == target) return busesTaken;
+
+                    for (int neighborRoute : stopToRoutes.getOrDefault(stop, new ArrayList<>())) {
+                        if (!visitedRoutes[neighborRoute]) {
+                            visitedRoutes[neighborRoute] = true;
+                            queue.offer(neighborRoute);
+                        }
+                    }
+                }
+            }
+
+            busesTaken++;
+        }
+
+        return -1;
+    }
+}
