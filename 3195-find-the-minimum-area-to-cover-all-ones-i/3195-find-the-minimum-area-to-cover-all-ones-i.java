484c8f91337fb599b6a4d308c2a2694411e2b4a7
Hangsha <125459168+hangsha-saha@users.noreply.github.com>
2025-08-22 18:01:57 +0530

    Time: 5 ms (94.31%), Space: 158.3 MB (85.37%) - LeetHub

---
+++ 3195-find-the-minimum-area-to-cover-all-ones-i/3195-find-the-minimum-area-to-cover-all-ones-i.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int minimumArea(int[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        int minRow = rows, maxRow = -1;
+        int minCol = cols, maxCol = -1;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == 1) {
+                    minRow = Math.min(minRow, i);
+                    maxRow = Math.max(maxRow, i);
+                    minCol = Math.min(minCol, j);
+                    maxCol = Math.max(maxCol, j);
+                }
+            }
+        }
+
+        if (maxRow == -1) return 0;
+
+        int height = maxRow - minRow + 1;
+        int width = maxCol - minCol + 1;
+        return height * width;
+    }
+}
